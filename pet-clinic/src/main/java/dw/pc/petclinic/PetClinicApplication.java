3d85e7e55e4e72569f4b50bcde38869413e4cb0c
Dan <dawolterbeek@gmail.com>
2018-12-17 10:14:32 -0500

    test

--- pet-clinic/src/main/java/dw/pc/petclinic/PetClinicApplication.java
+++ pet-clinic/src/main/java/dw/pc/petclinic/PetClinicApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PetClinicApplication {
 
+	//main
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
