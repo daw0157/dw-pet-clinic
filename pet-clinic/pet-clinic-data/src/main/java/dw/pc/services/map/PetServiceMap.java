2277e21f32964d295f2b9d925d94f2cff940242f
Dan <dawolterbeek@gmail.com>
2018-12-19 13:58:44 -0500

    map based services

---
+++ pet-clinic/pet-clinic-data/src/main/java/dw/pc/services/map/PetServiceMap.java
@@ -0,0 +1,40 @@
+package dw.pc.services.map;
+
+import java.util.Set;
+
+import dw.pc.model.Pet;
+import dw.pc.services.CrudService;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+	@Override
+	public Set<Pet> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Pet findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Pet save(Pet object) {
+		return super.save(object.getId(), object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Pet object) {
+		super.delete(object);
+	}
+
+
+	
+	
+	
+
+}
