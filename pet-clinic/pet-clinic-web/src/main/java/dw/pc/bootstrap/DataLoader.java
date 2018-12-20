b8250a557d87eadb9f6f7f1154598c3fc23b35ea
Dan <dawolterbeek@gmail.com>
2018-12-20 09:12:02 -0500

    auto generate ids, refactor services to accomodate

--- pet-clinic/pet-clinic-web/src/main/java/dw/pc/bootstrap/DataLoader.java
+++ pet-clinic/pet-clinic-web/src/main/java/dw/pc/bootstrap/DataLoader.java
@@ -23,40 +23,30 @@ public class DataLoader implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		
 		Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
-
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
-
         ownerService.save(owner2);
         
         Owner owner3 = new Owner();
-        owner3.setId(3L);
         owner3.setFirstName("Tom");
         owner3.setLastName("Brady");
-
         ownerService.save(owner3);
 
         System.out.println("Loaded Owners...." + ownerService.findAll().size());
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
-
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
-
         vetService.save(vet2);
 
 	    System.out.println("Loaded Vets...." + vetService.findAll().size());
