1f774660cc4a87e42fb8c305102356c126d0027c
Dan <dawolterbeek@gmail.com>
2018-12-21 14:50:12 -0500

    add initial JPA annotations

--- pet-clinic/pet-clinic-data/src/main/java/dw/pc/model/BaseEntity.java
+++ pet-clinic/pet-clinic-data/src/main/java/dw/pc/model/BaseEntity.java
@@ -2,10 +2,16 @@ package dw.pc.model;
 
 import java.io.Serializable;
 
-import lombok.Data;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
--- pet-clinic/pet-clinic-data/src/main/java/dw/pc/model/PetType.java
+++ pet-clinic/pet-clinic-data/src/main/java/dw/pc/model/PetType.java
@@ -1,10 +1,17 @@
 package dw.pc.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 	
 	public static final String DOG = "Dog";
 	public static final String CAT = "Cat";
 
+	@Column(name = "name")
 	private String name;
 
 	public String getName() {
