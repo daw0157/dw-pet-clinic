85cff77ab34d9cf5ef85179911f19b9663b850e7
Dan <dawolterbeek@gmail.com>
2018-12-21 15:49:49 -0500

    add remaining sdjpa

---
+++ pet-clinic/pet-clinic-data/src/main/java/dw/pc/services/springdatajpa/SpecialtyServiceSDJpa.java
@@ -0,0 +1,52 @@
+package dw.pc.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+
+import org.springframework.context.annotation.Profile;
+
+import dw.pc.model.Specialty;
+import dw.pc.repositories.SpecialtyRepository;
+import dw.pc.services.SpecialtyService;
+
+@Entity
+@Profile("springdatajpa")
+public class SpecialtyServiceSDJpa implements SpecialtyService {
+
+	private final SpecialtyRepository specialtyRepository;
+	
+	public SpecialtyServiceSDJpa(SpecialtyRepository specialtyRepository) {
+		super();
+		this.specialtyRepository = specialtyRepository;
+	}
+
+	@Override
+	public Set<Specialty> findAll() {
+		Set<Specialty> specialties = new HashSet<>();
+		specialtyRepository.findAll().forEach(specialties::add);
+		return specialties;
+	}
+
+	@Override
+	public Specialty findById(Long id) {
+		return specialtyRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Specialty save(Specialty object) {
+		return specialtyRepository.save(object);
+	}
+
+	@Override
+	public void delete(Specialty object) {
+		specialtyRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		specialtyRepository.deleteById(id);
+	}
+
+}
