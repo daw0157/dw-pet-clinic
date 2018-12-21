d3ed2f55ae625d192b63ecbc9e66dfcc478fb347
Dan <dawolterbeek@gmail.com>
2018-12-20 20:06:28 -0500

    create specialty, add to vet

---
+++ pet-clinic/pet-clinic-data/src/main/java/dw/pc/model/Specialty.java
@@ -0,0 +1,15 @@
+package dw.pc.model;
+
+public class Specialty extends BaseEntity {
+	
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
